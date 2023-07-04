8b4198bf223b60d2d9be8d4737b70c03c89bca10
beyond88 <muhin.cse.diu@gmail.com>
2023-07-04 10:32:37 +0600

    add: ajax class

---
+++ includes/Helper.php
@@ -0,0 +1,22 @@
+<?php
+namespace SeoCrawl;
+
+/**
+* Frontend handler class
+*
+* @since    1.0.0
+* @param    none
+* @return   object
+*/
+class Helper {
+
+    /**
+    * Initialize the class
+    *
+    * @since    1.0.0
+    * @param    none
+    * @return   object
+    */
+    function __construct() {
+    }
+}
